4f4e3e884f8adb2540a7002825483952735f6c27
desirè passalacqua <desirepassalacqua@MacBook-Pro-di-Desy.local>
2023-12-04 01:02:15 +0100

    ex7

---
+++ exercise.js
@@ -0,0 +1,10 @@
+const figlet = require("figlet");
+
+figlet("What's Up?!?", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
